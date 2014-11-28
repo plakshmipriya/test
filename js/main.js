fea470a3c266735f02fb8975c8ebb17e42f78a57
plakshmipriya <plakshmipriya@gmail.com>
2014-11-28 22:25:04 +0530

    Part9Changes

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 var app = {
 	registerEvents: function() {
 		var self = this;
+		$(window).on('hashchange', $.proxy(this.route, this));
     // Check of browser supports touch events...
 		if (document.documentElement.hasOwnProperty('ontouchstart')) {
 			// ... if yes: register touch event listener to change the "selected" state of the item
@@ -37,6 +38,19 @@ var app = {
             }
         });
     },*/
+	route: function() {
+		var hash = window.location.hash;
+			if (!hash) {
+				$('body').html(new HomeView(this.store).render().el);
+				return;
+			}
+		var match = hash.match(app.detailsURL);
+			if (match) {
+				this.store.findById(Number(match[1]), function(employee) {
+				$('body').html(new EmployeeView(employee).render().el);
+			});
+		}
+	}
 	showAlert: function (message, title) {
 		if (navigator.notification) {
 			navigator.notification.alert(message, null, title, 'OK');
@@ -60,9 +74,10 @@ var app = {
 		});
 		*/
 		 var self = this;
+		 this.detailsURL = /^#employees\/(\d{1,})/;
 	     this.registerEvents();
 		 this.store = new MemoryStore(function() {
-			$('body').html(new HomeView(self.store).render().el);
+			self.route();
 		});
         //$('.search-key').on('keyup', $.proxy(this.findByName, this));
     }
